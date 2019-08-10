94f935f0eea71e36ca04abe6609246028fa96229
Ignacio <ignacio.hdz.cz@gmail.com>
2019-08-10 02:50:04 -0500

    estructura app

---
+++ src/main/java/com/shedule/app/ScheduledAppApplication.java
@@ -0,0 +1,13 @@
+package com.shedule.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shedule/app/ScheduledAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shedule.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
