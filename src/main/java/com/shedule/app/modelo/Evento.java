bfb88dcb7de5e564adbb9af3735a5f4d826455ae
Ignacio <ignacio.hdz.cz@gmail.com>
2019-08-10 12:52:25 -0500

    Agregando dependecias y conexion a base de datos

--- src/main/java/com/shedule/app/modelo/Evento.java
+++ src/main/java/com/shedule/app/modelo/Evento.java
@@ -1,6 +1,21 @@
 package com.shedule.app.modelo;
 
-public class Evento {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long codigo;
+	 
+	
 	
 	private String nombre;
 	private String lugar;
@@ -31,4 +46,12 @@ public class Evento {
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
+	public long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+	
+	
 }
